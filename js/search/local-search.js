b357cb5dffb094af812b3f7cd5c6054997b55309
hongdouzza <146096467+hongdouzza@users.noreply.github.com>
2023-11-19 02:48:04 +0000

    🎉 INT COMMIT: 更改看板娘手机展示，百度推送删 Updated By Github Actions

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -1,4 +1,1 @@
-const searchClickFn = () => {
-    document.querySelector('#search-button > .search').addEventListener('click', openSearch)
-    document.querySelector('#menu-search').addEventListener('click', openSearch)
-  }
+const searchClickFn=()=>{document.querySelector("#search-button > .search").addEventListener("click",openSearch),document.querySelector("#menu-search").addEventListener("click",openSearch)};
