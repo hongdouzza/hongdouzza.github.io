b15d15880f9b45ed1bc5da74fa62c88f160406da
hongdouzza <1747480774@qq.com>
2024-04-11 14:07:40 +0800

    Site updated: 2024-04-11 14:07:40

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -1,4 +1,4 @@
 const searchClickFn = () => {
     document.querySelector('#search-button > .search').addEventListener('click', openSearch)
-    document.querySelector('#menu-search').addEventListener('click', openSearch)
+    
   }
